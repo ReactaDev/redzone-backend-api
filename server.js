b7e5398fff9522e62c6f51c16cd87564e3ada1f5
Owen Ackner <owen.ackner@gmail.com>
2023-04-24 09:07:41 -0400

    test

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.use(express.json())
+
+//server code here
+
+app.post('/test', (req, res) => {
+    const body1 = req.body.message
+
+    res.send(body1)
+})
+
+
+
+//listen on port
+
+app.listen(8080)
