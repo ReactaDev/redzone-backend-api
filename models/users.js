55280849f82cc3d57279f4ef23f57146da0d1713
Owen Ackner <owen.ackner@gmail.com>
2023-04-27 11:40:01 -0400

    .gitignore

---
+++ models/users.js
@@ -0,0 +1,11 @@
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema;
+
+var users = new Schema {
+
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: St
+
+}
