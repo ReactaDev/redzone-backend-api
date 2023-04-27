599ed41cb30428fa781e529a864a6f9c3eb828a3
Owen Ackner <owen.ackner@gmail.com>
2023-04-27 16:16:59 -0400

    Added Users Model

--- models/users.js
+++ models/users.js
@@ -1,11 +1,13 @@
 let mongoose = require('mongoose')
 let Schema = mongoose.Schema;
 
-var users = new Schema {
+var users = new Schema({
 
     firstName: String,
     lastName: String,
     email: String,
-    password: St
+    password: String
 
-}
+})
+
+module.exports = mongoose.model('users', users)
