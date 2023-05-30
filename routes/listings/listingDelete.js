764745ee2549e16b34e359e83113b33fdb5189cf
Owen Ackner <owen.ackner@gmail.com>
2023-05-30 11:28:54 -0400

    Added Listing Delete

---
+++ routes/listings/listingDelete.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.use(express.json())
+//import model
+const Listings = require('../../models/listings')
+
+//posts
+
+//delete
+router.delete('/:listingID', async (req, res) =>{
+
+    const listingID = Listings.find(req.params._id)
+    res.send('Listing Deleted')
+
+    const deletedListing = await Listings.deleteOne({_id: req.params.listingID})
+
+})
+
+
+
+
+
+//exports
+module.exports = router
