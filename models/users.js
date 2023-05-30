3ea22d291af75f752b32999509f3ac8f217c449b
Owen Ackner <owen.ackner@gmail.com>
2023-05-30 11:59:39 -0400

    Reformated Comments

--- models/users.js
+++ models/users.js
@@ -2,12 +2,10 @@ let mongoose = require('mongoose')
 let Schema = mongoose.Schema;
 
 var users = new Schema({
-
     firstName: String,
     lastName: String,
     email: String,
     password: String
-
 })
 
 module.exports = mongoose.model('users', users)
